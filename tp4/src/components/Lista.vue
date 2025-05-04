f0fd5dc26865f3e4ce4032dfa1ee5d96cfcfbe1d
Franco Narvaez <juanfranconarvaez@gmail.com>
2025-05-04 15:55:51 -0300

    Agrego un loader spinner

--- tp4/src/components/Lista.vue
+++ tp4/src/components/Lista.vue
@@ -4,7 +4,8 @@
         <ul v-if="items.length">
             <li v-for="(item, index) in items" :key="item.id">{{ item.nombre }}</li>
         </ul>
-        <p v-else>Cargando...</p>
+        <Loader v-else>
+        </Loader>
     </div>
 </template>
 
@@ -12,6 +13,7 @@
 import { ref, onMounted } from 'vue';
 import type { Profesion } from '../interfaces/Profesion';
 import { profesiones } from '../data/profesiones';
+import Loader from './Loader.vue';
 
 const items = ref<Profesion[]>([]);
 
